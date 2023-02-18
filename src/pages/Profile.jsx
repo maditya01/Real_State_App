cc112e416cc5691dad6d549499c5ba792f31dda9
Aditya Maurya <2082001aditya@gmail.com>
2023-02-18 10:36:23 +0530

    added edit profile functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router";
-import { getAuth } from "firebase/auth";
-
+import { getAuth, updateProfile } from "firebase/auth";
+import { toast } from "react-toastify";
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../firebase";
 const Profile = () => {
+  const [changeDetail, setChangeDetail] = useState(false);
   const navigate = useNavigate();
   const auth = getAuth();
   const [formData, setFormData] = useState({
@@ -14,19 +17,44 @@ const Profile = () => {
     auth.signOut();
     navigate("/");
   }
+  function onChange(e) {
+    setFormData((prevD) => ({
+      ...prevD,
+      [e.target.id]: e.target.value,
+    }));
+  }
+  async function onSubmit() {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        //update display name in firebase auth.
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+
+        //update display name in fire-store db.
+        const docRef = doc(db, "users", auth.currentUser.uid);
+        await updateDoc(docRef, { name });
+        toast.success("Profile Details updated");
+      }
+    } catch (error) {
+      toast.error("Could not apply changes");
+    }
+  }
   return (
     <>
       <section className="max-w-6xl mx-auto flex justify-center items-center flex-col">
         <h1 className="text-3xl text-center font-bold mt-6">My Profile</h1>
         <div className="w-full md:w-[50%] mt-6 px-3">
-          <form>
+          <form onSubmit={onSubmit}>
             <input
-              disabled
+              onChange={onChange}
+              disabled={!changeDetail}
               type="text"
               id="name"
               value={name}
-              className="mb-6 w-full px-4 py -2 text-xl text-gray-700
-               bg-white border-gray-300 transition ease-in-out"
+              className={`mb-6 w-full px-4 py -2 text-xl text-gray-700
+               bg-white border-gray-300 transition ease-in-out
+               ${changeDetail && "bg-red-200 focus:bg-red-200"}`}
             />
             <input
               disabled
@@ -39,8 +67,13 @@ const Profile = () => {
             <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
               <p className="flex items-center">
                 Do you want to change name?
-                <span className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer ">
-                  Edit
+                <span
+                  onClick={() => {
+                    setChangeDetail((prevS) => !prevS);
+                  }}
+                  className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer "
+                >
+                  {changeDetail ? "Apply Changes" : "Edit"}
                 </span>
               </p>
               <p
